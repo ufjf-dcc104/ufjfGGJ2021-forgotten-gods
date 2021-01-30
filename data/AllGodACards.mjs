520a57e002040d6ed4d5b6b9cc3c189944963be7
Igor Knop <igorknop@gmail.com>
2021-01-30 11:49:29 -0300

    Fix constant typo

--- data/AllGodACards.mjs
+++ data/AllGodACards.mjs
@@ -1,5 +1,5 @@
 import { PRIEST, SOLDIER, FARMER, SENATOR } from "../util/peopleTypes.mjs";
-export const ALL_GODA_CARDS = [
+export const ALL_GOD_A_CARDS = [
   { demands: [SENATOR], type: 0, qty: 1 },
   { demands: [SOLDIER], type: 0, qty: 1 },
   { demands: [FARMER], type: 0, qty: 1 },
