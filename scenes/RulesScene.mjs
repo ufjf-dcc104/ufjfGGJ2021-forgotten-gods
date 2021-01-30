d1ee53ecfab99bd662d767875a9f1a0229924e0a
Luís Cavalheiro <luiscv86@gmail.com>
2021-01-30 20:51:56 -0300

    Criada a classe RulesScene

---
+++ scenes/RulesScene.mjs
@@ -0,0 +1,106 @@
+import Button from "../Button.mjs";
+
+export default class RulesScene {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.expire = 180;
+    this.grace = 5;
+    this.reputation = 5;
+    this.dragging = null;
+    this.t0;
+    this.dt;
+    this.areas = {};
+    const touches = [];
+    this.createAreas();
+  }
+  start() {
+    requestAnimationFrame((t) => {
+      this.step(t);
+    });
+  }
+
+  setup() {
+    this.canvas.onmousedown = (e) => {
+      this.mousedown(e);
+    };
+    this.canvas.onmouseup = (e) => {
+      this.mouseup(e);
+    };
+    this.canvas.onclick = (e) => {
+      this.click(e);
+    };
+    this.canvas.onmousemove = (e) => {
+      this.mousemove(e);
+    };
+    this.canvas.onmouseout = (e) => {
+      this.mouseout(e);
+    };
+    this.canvas.ontouchstart = (e) => {
+      this.touchstart(e);
+    };
+    this.canvas.ontouchend = (e) => {
+      this.touchend(e);
+    };
+    this.canvas.ontouchmove = (e) => {
+      this.touchmove(e);
+    };
+  }
+
+  step(t) {
+    this.t0 = this.t0 ?? t;
+    this.dt = (t - this.t0) / 1000;
+    this.ctx.fillStyle = "hsl(200, 7%, 84%)";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.strokeStyle = "hsl(200, 7%, 74%)";
+    this.ctx.strokeRect(0, 0, this.canvas.width, this.canvas.height);
+    this.mainMenu.draw(this.ctx);
+    this.ctx.fillStyle = "black";
+    this.ctx.font = "50px bold monospace";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText(`Rules`, this.canvas.width / 2, 0.2 * this.canvas.height );
+    requestAnimationFrame((t) => {
+      this.step(t);
+    });
+    this.t0 = t;
+  }
+
+  createAreas() {
+    this.mainMenu = new Button(
+      0.5 * this.canvas.width,
+      0.625 * this.canvas.height,
+      0.25 * this.canvas.width,
+      0.05357142857142857 * this.canvas.height,
+      "Main Menu"
+    );
+  }
+
+  mousedown(e) {
+    const x = e.pageX - this.canvas.offsetLeft;
+    const y = e.pageY - this.canvas.offsetTop;
+    if (this.mainMenu.hasPoint({ x, y })) {
+      this.game.setScene("start");
+    }
+  }
+  mouseup(e) {}
+  click(e) {
+    const x = e.pageX - this.canvas.offsetLeft;
+    const y = e.pageY - this.canvas.offsetTop;
+  }
+  mousemove(e) {}
+  mouseout(e) {}
+
+  touchstart(e) {
+    const newTouch = e.changedTouches[0];
+    this.mousedown(newTouch);
+  }
+  touchend(e) {
+    const newTouch = e.changedTouches[0];
+    this.mouseup(newTouch);
+  }
+  touchmove(e) {
+    e.preventDefault();
+    const newTouch = e.changedTouches[0];
+    this.mousemove(newTouch);
+  }
+}
