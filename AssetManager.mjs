6c97c4b654999eb15389332e951da80ee854957f
Igor Knop <igorknop@gmail.com>
2021-01-30 15:09:53 -0300

    Add assetmanager

--- AssetManager.mjs
+++ AssetManager.mjs
@@ -1,2 +1,77 @@
 export const bg = new Image();
 bg.src = "./assets/gamejam.png";
+
+
+export default class AssetManager {
+  constructor() {
+    this.aCarregar = 0;
+    this.carregadas = 0;
+    this.images = {};
+    this.audios = {};
+    this.channels = [];
+    this.MAX_CHANNELS = 20;
+    for (var i = 0; i < this.MAX_CHANNELS; i++) {
+      this.channels[i] = {
+        audio: new Audio(),
+        fim: -1,
+      };
+    }
+  }
+
+  loadImage(key, url) {
+    console.log(`Carregando imagem ${url}...`);
+
+    this.aCarregar++;
+    var imagem = new Image();
+    imagem.src = url;
+    this.images[key] = imagem;
+    var that = this;
+    imagem.addEventListener("load", function () {
+      that.carregadas++;
+      console.log(
+        `Imagem ${that.carregadas}/${that.aCarregar} ${key}: ${url} carregada.`
+      );
+    });
+  }
+
+  img(key) {
+    return this.images[key];
+  }
+
+  progresso() {
+    if (this.aCarregar != 0) {
+      return (this.carregadas / this.aCarregar) * 100.0;
+    } else return 0.0;
+  }
+
+  loadAudio(key, url) {
+    console.log(`Carregando audio ${key}: ${url}...`);
+    //this.aCarregar++;
+    var audio = new Audio();
+    audio.src = url;
+    audio.load();
+    this.audios[key] = audio;
+    var that = this;
+    /*audio.addEventListener("canplay", function () {
+            //that.carregadas++;
+            console.log(`Audio ${that.carregadas}/${that.aCarregar} ${key}: ${url} carregado.`);
+        });
+        */
+  }
+
+  play(key) {
+    if (!this.audios[key]) {
+      throw new Error(`Chave de audio inválida: ${key}!`);
+    }
+    for (var i = 0; i < this.MAX_CHANNELS; i++) {
+      var agora = new Date();
+      if (this.channels[i].fim < agora.getTime()) {
+        this.channels[i].audio.src = this.audios[key].src;
+        this.channels[i].fim =
+          agora.getTime() + this.audios[key].duration * 1000;
+        this.channels[i].audio.play();
+        break;
+      }
+    }
+  }
+}
--- Game.mjs
+++ Game.mjs
@@ -1,9 +1,17 @@
 import EndScene from "./EndScene.mjs";
 import StartScene from "./StartScene.mjs";
 import GameScene from "./GameScene.mjs";
+import AssetManager from "./AssetManager.mjs";
 
 export default class Game {
   constructor(canvas) {
+    this.assets = new AssetManager();
+    this.assets.loadImage("bg", "./assets/gamejam.png");
+    this.assets.loadImage("priest", "./assets/Priest.png");
+    this.assets.loadImage("farmer", "./assets/Farmer.png");
+    this.assets.loadImage("soldier", "./assets/soldier.png");
+    this.assets.loadImage("senator", "./assets/politician.png");
+
     this.scenes = new Map();
     this.addScene("game", new GameScene(canvas));
     this.addScene("end", new EndScene(canvas));
@@ -12,11 +20,12 @@ export default class Game {
   }
   addScene(key, scene) {
     scene.game = this;
+    scene.assets = this.assets;
     this.scenes.set(key, scene);
   }
   setScene(scene) {
     if (this.scenes.has(scene)) {
-        console.log(scene);
+      console.log(scene);
       this.scene = this.scenes.get(scene);
       this.setup();
       this.start();
