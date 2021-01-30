d59874844883862271606bc8ae4c9bc428b3524d
Caio <caio.vincenzo@estudante.ufjf.br>
2021-01-30 15:44:11 -0300

    Activities position to relative

--- Activities.mjs
+++ Activities.mjs
@@ -3,7 +3,7 @@ import People from "./People.mjs";
 import { shuffleArray } from "./util/shuffle.mjs";
 
 export default class Activities {
-  constructor(x = 300, y = 120, type=0) {
+  constructor(x = 300, y = 120, type = 0) {
     this.x = x;
     this.y = y;
     this.activities = [];
@@ -15,8 +15,8 @@ export default class Activities {
 
   loadAll(activities) {
     activities.forEach((s) => {
-      for (let c = 0; c < s.qty; c++) {        
-        this.add(new Activity(s.demands, s.type, s.expire, s.effect));
+      for (let c = 0; c < s.qty; c++) {
+        this.add(new Activity({ ...s, w: 200, h: 200 }));
       }
     });
     this.shuffle();
@@ -32,24 +32,40 @@ export default class Activities {
   }
 
   draw(ctx) {
+    const canvas = ctx.canvas;
     for (let s = 0; s < Math.min(this.activities.length, this.max); s++) {
       const activity = this.activities[s];
       activity.x = this.x + (activity.w + 8) * s;
       activity.y = this.y;
       activity.draw(ctx);
-      const y = this.y + 70;
-      const x = this.x - 30;
+      const x = this.x - 0.09375 * canvas.width;
+      const y = this.y + 0.125 * canvas.height;
       ctx.fillStyle = "white";
-      ctx.fillRect(x, y - 2, 60, 9);
+      ctx.fillRect(
+        x,
+        y,
+        0.1875 * canvas.width,
+        0.01607142857142857 * canvas.height
+      );
       ctx.fillStyle = "black";
       ctx.fillStyle = "yellow";
-      ctx.fillRect(x, y, (60 * (1 - this.spawn)), 5);
+      ctx.fillRect(
+        x,
+        y,
+        0.1875 * canvas.width * (1 - this.spawn),
+        0.008928571428571428 * canvas.height
+      );
       ctx.strokeStyle = "black";
-      ctx.strokeRect(x, y - 2, 60, 9);
+      ctx.strokeRect(
+        x,
+        y - 2,
+        0.1875 * canvas.width,
+        0.01607142857142857 * canvas.height
+      );
       ctx.fillStyle = `hsl(${(240 * this.reputation) / 4}, 100%, 50%)`;
       ctx.beginPath();
       ctx.ellipse(
-        x + (this.reputation * 60) / 4,
+        x + (this.reputation * 0.1875 * canvas.width) / 4,
         y + 3,
         3,
         3,
@@ -63,7 +79,7 @@ export default class Activities {
   }
 
   expire(dt, game) {
-    this.spawn -= (this.reputation + 1)/60 * dt;
+    this.spawn -= ((this.reputation + 1) / 60) * dt;
     if (this.spawn <= 0) {
       game.areas.available.people.push(new People(this.type));
       this.spawn = 1;
@@ -82,13 +98,13 @@ export default class Activities {
   check(x, y) {
     return this.activities.find((sac) => sac.hasPoint({ x, y }));
   }
-  sendToBottom(activity){
+  sendToBottom(activity) {
     const idx = this.activities.indexOf(activity);
     const act = this.activities.splice(idx, 1)[0];
     this.activities.push(act);
   }
 
-  shuffle(){
+  shuffle() {
     shuffleArray(this.activities);
   }
 }
