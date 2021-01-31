752880d7b980598a90924f6fc6dcf02aa6ea0f1a
Igor Knop <igorknop@gmail.com>
2021-01-30 21:21:31 -0300

    Add god B cards

---
+++ data/AllGodBCards.mjs
@@ -0,0 +1,7 @@
+import { PRIEST, SOLDIER, FARMER, SENATOR , SLOW } from "./AllTimeConstants.mjs";
+export const ALL_GOD_B_CARDS = [
+  { demands: [SENATOR], expire: SLOW, type: SENATOR, qty: 1 },
+  { demands: [SOLDIER], expire: SLOW, type: SOLDIER, qty: 1 },
+  { demands: [FARMER], expire: SLOW, type: FARMER, qty: 1 },
+  { demands: [PRIEST], expire: SLOW, type: PRIEST, qty: 1 },
+];
